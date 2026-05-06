03c933269f1bcf7f1190b223f44e652864b5ac97
dyhptri <diyahrp10@gmail.com>
2026-05-06 14:38:26 +0700

    Praktikum 1 langkah 5

--- lib/main.dart
+++ lib/main.dart
@@ -41,8 +41,19 @@ class _FuturePageState extends State<FuturePage> {
           children: [
             const Spacer(),
             ElevatedButton(
-              child: const Text('GO!'), 
-              onPressed: () {}
+              child: const Text('GO!'),
+              onPressed: () {
+                setState(() {});
+                getData()
+                    .then((value) {
+                      result = value.body.toString().substring(0, 450);
+                      setState(() {});
+                    })
+                    .catchError((_) {
+                      result = 'An error occurred';
+                      setState(() {});
+                    });
+              },
             ),
             const Spacer(),
             Text(result),
