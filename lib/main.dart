527f1b5059b5ec56ff3ec77c7a833e4bdb74be8d
dyhptri <diyahrp10@gmail.com>
2026-05-06 15:08:26 +0700

    Praktikum 3 langkah 1 - 4

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +33,20 @@ class FuturePage extends StatefulWidget {
 
 class _FuturePageState extends State<FuturePage> {
   String result = '';
+
+  late Completer completer;
+
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future calculate() async {
+    await Future.delayed(const Duration(seconds: 5));
+    completer.complete(42);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,17 +58,11 @@ class _FuturePageState extends State<FuturePage> {
             ElevatedButton(
               child: Text('GO!'),
               onPressed: () {
-                count();
-                // setState(() {});
-                // getData()
-                //     .then((value) {
-                //       result = value.body.toString().substring(0, 450);
-                //       setState(() {});
-                //     })
-                //     .catchError((_) {
-                //       result = 'An error occurred';
-                //       setState(() {});
-                //     });
+                getNumber().then((value) {
+                  setState(() {
+                    result = value.toString();
+                  });
+                });
               },
             ),
             const Spacer(),
